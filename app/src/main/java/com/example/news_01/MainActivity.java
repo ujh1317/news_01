15ee12fd18d5b82347d9806067fb928eb59bf40b
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-06-25 23:57:29 +0900

    6/25 debugging

--- app/src/main/java/com/example/news_01/MainActivity.java
+++ app/src/main/java/com/example/news_01/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.my_recycler_view);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
